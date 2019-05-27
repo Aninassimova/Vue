4a42a887dc9e36c91b3c48515a0266648bd1d123
Aninassimova <1402387648@qq.com>
2019-05-28 00:48:12 +0800

    平行通信

---
+++ 13_组件通信之任意及平行组件间通信/js/main.js
@@ -0,0 +1,37 @@
+var Event=new Vue(); //调度器
+
+//平行组件
+Vue.component('huahua',{
+	template:'<div>我说：<input @keyup="on_change" v-model="i_said"></div>',
+	methods:{
+		on_change:function(){
+			Event.$emit('huahua-said-something',this.i_said);  //emit(事件，数据）
+		}
+	},
+	data:function(){
+		return{
+			i_said:'',
+		}
+	}
+});
+Vue.component('dandan',{
+	template:'<div>花花说：{{huahua_said}}</div>',
+	data:function(){
+		return{
+			huahua_said:'',
+		};
+	},
+	mounted:function(){  //初始化完毕
+		var me=this;
+		Event.$on('huahua-said-something',function(data){    //监听
+			//console.log("data",data);
+			//console.log("this",this);//this此时的域不再是component，而是调度器
+			me.huahua_said=data;
+		});
+	}
+});
+
+//域
+new Vue({
+	el:"#app",
+})
